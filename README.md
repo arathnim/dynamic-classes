7763f92edb638cc02f822887db7a62657c17eaac
arathnim <arathnim@gmail.com>
2017-01-24 13:19:14 -0600

    all done! (expect for the testing, whoops)

--- README.md
+++ README.md
@@ -3,3 +3,29 @@ Little library to make using CLOS significantly easier.
 Expect it to be replaced by an even more advanced library at some point.
 
 ### usage
+
+Because dynamic-classes defines it's own defclass and make-instance, you have to shadow the symbols in your own package to use them.
+
+```cl
+(defpackage my-package
+   (:shadowing-import-from dynamic-classes defclass make-instance)
+	(:use cl))
+```
+
+Then, just use the new syntax in normal defclass and make-instance calls.
+
+```cl
+(defclass square (shape drawable) :class-option value
+   (x 0 :type fixnum)
+   (y 0 :type fixnum)
+   (side-length 10)
+   
+   (draw ((this square))
+      (with-slots (x y side-length) this
+         (draw-rectangle +blue+ x y side-length side-length))))
+
+(make-instance square :x 20 :y 50
+   (draw ((this square))
+      (with-slots (x y side-length) this
+         (draw-rounded-rectangle +red+ x y side-length side-length))))
+```
--- dynamic-classes.lisp
+++ dynamic-classes.lisp
@@ -17,18 +17,36 @@
    (intern (symbol-name symbol) :keyword))
 
 (defun expand-slot (name value &rest more-keys)
-	`(,name :accessor ,name :initarg ,(as-keyword name) :initform ,value ,@more-keys))
+   `(,name :accessor ,name :initarg ,(as-keyword name) :initform ,value ,@more-keys))
 
 (defun expand-method (name args &rest body)
-	`(cl:defmethod ,name ,args ,@body))
+   `(cl:defmethod ,name ,args ,@body))
 
 (defmacro defclass (name superclass &rest attributes)
-	(let ((slots nil) (methods nil))
-		(iter (for x in attributes)
-			(when (listp x)
-				(if (or (eql (length x) 2) (and (eql (length x) 3) (keywordp (nth 3 x))))
-					 (push (apply #'expand-slot x) slots)
-					 (push (apply #'expand-method x) methods))))
-		`(progn 
-			(cl:defclass ,name ,superclass (,@slots))
-			,@methods)))
+   (let ((slots nil) (methods nil))
+      (iter (for x in attributes)
+         (when (listp x)
+            (if (or (eql (length x) 2) (and (eql (length x) 3) (keywordp (nth 3 x))))
+                (push (apply #'expand-slot x) slots)
+                (push (apply #'expand-method x) methods))))
+      `(progn 
+         (cl:defclass ,name ,superclass (,@slots))
+         ,@methods)))
+
+(defvar *gensym* (make-hash-table :test #'equalp))
+(defun gen-sym (&optional (prefix "gs"))
+   (setf (gethash prefix *gensym*) (+ 1 (gethash prefix *gensym* 0)))
+   (intern (string-upcase (format nil "~a~a" prefix (gethash prefix *gensym*)))))
+
+;; guess I'll just do this manually
+;; TODO: symbol/list parser combinators
+;; ahahaha, generator magic
+(defmacro make-instance (class &rest args)
+   (let ((keyargs nil) (defs nil) (gs (gen-sym "dynamic-class"))) 
+      (iter (generating x in args)
+            (if (keywordp x)
+                (progn (push (list x (next x)) keyargs) (next x))
+                (progn (when x (push x defs)) (next x))))
+      (if defs
+         `(progn (defclass ,gs (,class) ,@defs) (cl:make-instance ',gs ,@(flatten keyargs)))
+         `(cl:make-instance ',class ,@(flatten keyargs)))))
